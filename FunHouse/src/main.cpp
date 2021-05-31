6905ffef1745a55edfccd32b7a3823be830d6e93
Isaac Tait <isaac@mountaintopcoding.com>
2021-05-31 09:42:04 -0700

    add serial.println

--- FunHouse/src/main.cpp
+++ FunHouse/src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Simple Blink sketch
+  simple-blink.cpp
+  Use for PlatformIO demo
+
+  From original Arduino Blink Sketch
+  https://www.arduino.cc/en/Tutorial/Blink
+  
+  DroneBot Workshop 2021
+  https://dronebotworkshop.com
+*/
+
+// Set LED_BUILTIN if undefined or not pin 13
+// #define LED_BUILTIN 13
+
+void setup()
+{
+  // Initialize LED pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Set the LED HIGH
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("HIGH");
+
+  // Wait for a second
+  delay(1000);
+
+  // Set the LED LOW
+  digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("LOW");
+
+   // Wait for a second
+  delay(1000);
 }
